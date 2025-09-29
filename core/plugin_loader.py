c67081ca7cdcd90f0b83ba3faf59a6eb05b5c70c
Peter Schramm <peter_schramm@live.de>
2025-09-30 00:04:53 +0200

    Made imports possible for executable

--- core/plugin_loader.py
+++ core/plugin_loader.py
@@ -1,14 +1,19 @@
+import sys
 import os
 import importlib
 from . import plugin_registry 
 from .plugin import PluginInterface
 
 def import_module(name: str) -> PluginInterface:
+    print(f"importing {name}")
     return importlib.import_module(name)
 
-def load_from_dir(plugin_path: str):
-    plugins = [p for p in os.listdir(plugin_path) if p.endswith(".py")]
+def load_from_dir(plugin_dir: str):
+    os.makedirs(plugin_dir, exist_ok=True)
+    print(os.path.realpath(plugin_dir))
+    sys.path.insert(0, os.path.realpath(plugin_dir))
+    plugins = [p for p in os.listdir(plugin_dir) if p.endswith(".py")]
     for plugin_name in plugins:
         plugin_name, ext = os.path.splitext(plugin_name)
-        mod = import_module(f"plugins.{plugin_name}")
+        mod = import_module(f"{plugin_name}")
         plugin_registry.register(plugin_name, mod)
--- core/plugin_registry.py
+++ core/plugin_registry.py
@@ -7,4 +7,6 @@ def register(plugin_name: str, plugin):
     registered[plugin_name] = plugin
 
 def call(plugin: str):
+    if plugin not in registered:
+        raise ValueError(f"Plugin {plugin} not found")
     registered[plugin].func()
